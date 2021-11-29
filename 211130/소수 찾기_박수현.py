54baf9b408aa31766beb668d9c5109a9edb36e85
pySoo <55135881+pySoo@users.noreply.github.com>
2021-11-29 14:56:11 +0900

    Update 소수 찾기_박수현

--- 211130/소수 찾기_박수현.py
+++ 211130/소수 찾기_박수현.py
@@ -1,10 +1,20 @@
 """
 https://programmers.co.kr/learn/courses/30/lessons/42839
 
+정확성  테스트
+테스트 1 〉	통과 (0.06ms, 10.4MB)
+테스트 2 〉	통과 (2.28ms, 10.5MB)
+테스트 3 〉	통과 (0.03ms, 10.5MB)
+테스트 4 〉	통과 (0.53ms, 10.5MB)
+테스트 5 〉	통과 (3.15ms, 10.8MB)
+테스트 6 〉	통과 (0.03ms, 10.5MB)
+테스트 7 〉	통과 (0.07ms, 10.5MB)
+테스트 8 〉	통과 (3.12ms, 11MB)
+테스트 9 〉	통과 (0.04ms, 10.5MB)
+테스트 10 〉	통과 (5.43ms, 10.5MB)
+테스트 11 〉	통과 (0.54ms, 10.5MB)
+테스트 12 〉	통과 (0.18ms, 10.5MB)
 
-
-"""
-"""
 조건
 - 숫자를 조합하여 만들 수 있는 소수 개수 구하기
 input: 종이 조각에 들어있는 숫자(string)
@@ -15,14 +25,10 @@ output: 조합으로 만들 수 있는 소수 개수(int)
 2. 제곱근으로 중앙에 위치한 약수를 구하고, 그 이하의 수들이 소수인지 판별
 3. 소수일 때마다 +1을 하고 합계 리턴
 """
-
-
+from itertools import permutations
 # 2
 
 
-
-
-from itertools import permutations
 def isPrime(n):
     if n in (0, 1):
         return False
